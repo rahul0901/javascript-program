291a33cf21d58023df34ccdc840f5fab53ea2192
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-08-13 13:43:41 +0530

    updtaes

--- Datatypes/index.js
+++ Datatypes/index.js
@@ -1,1 +1,39 @@
 console.log('xyz')
+
+// Data types-
+
+// 1) String
+// syntax = "rahul", 'Shinde', `44`
+
+// 2) Number
+// ex: 23, 678, 78.90
+
+// 3) Boolean
+// syntax = true, false
+
+// To store data we have variables..
+// three types of variables var, let, const..
+
+var myName = "Rahul";
+var mySurname = "Shinde";
+var myAge = 44;
+console.log(myName + ' ' + mySurname)
+
+var Name = "Swift";
+console.log(Name + " is a " + typeof(Name))
+
+var Name2 = 467649;
+console.log(Name2 + " is a " + typeof(Name2))
+
+var Name3 = true;
+console.log(Name3 + " is a " + typeof(Name3))
+
+// Template Literals
+
+var person = "ram";
+var dynamicdata = 'vbjvowdl';
+var message = "Hello Mr. Ram";
+var dynamicMessage = `hello Mr. ${dynamicdata}`;
+console.log(message)
+console.log(dynamicMessage)
+
