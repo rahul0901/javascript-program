cde78b3a7174527d9a080175d4b75c8b616369fb
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-09-02 17:43:29 +0530

    updates

---
+++ Practice 2/test2.js
@@ -0,0 +1,100 @@
+// Q1)
+// var arr = [2, 4, 5, 7, 8, 9];
+// var origin = arr[0];
+// var newArray = [];
+
+// function sumOfAllElements(array) {
+//     for (var i = 1; i < array.length; i++) {
+//         origin+=array[i]
+//         // newArray.push(origin + array[i])
+//     }
+//     return origin
+// }
+
+// console.log(sumOfAllElements(arr))
+
+// *********************************************************************
+// Q2)
+// var arr = [2, 4, 5, 7, 8, 9, 14];
+// var max = arr[0];
+
+// function findMax(array){
+//     for (var i = 1; i<array.length; i++){
+//         // console.log(array[i])
+//         if(max < array[i]){  //...2< 4
+//             max = array[i]
+//         }
+//     }
+
+//     return max
+// }
+// console.log(findMax(arr))
+
+
+
+// if(2<4){
+//     console.log("hi")
+// }
+
+// *********************************************************************
+// Q5)
+// var arr = [2, 4, 5, 7, 8, 9, 6, 10, 14];
+// var evenArray = [];
+// var evenCount = 0;
+
+// function countEven(array){
+//     for (var i = 0; i<array.length; i++){
+//         if (array[i] % 2 == 0){
+//             evenArray.push(array[i])
+//             // console.log(array[i])
+//             // console.log(evenArray)
+//         }
+//     }
+//     evenCount += evenArray.length;
+//     return evenCount
+// }
+
+// console.log(countEven(arr))
+
+// *********************************************************************
+//Q3)
+// var no = [2, 4, 5, 7, 8]  //..26
+// var no = [1,2,3,4,5,6,7,8]   //..36
+// var avg = 0;
+
+// function findAvg(array) {
+//     for (var i = 0; i < no.length; i++) {
+//         avg += no[i];
+//     }
+//     avg = avg/no.length;
+//     return avg
+// }
+
+// console.log(findAvg(no))
+
+
+// for (var i = 0; i<no.length; i++){
+//     avg+= no[i];
+// }
+
+// console.log(avg/no.length)
+
+// *********************************************************************
+
+// Q4) reverse
+
+var arr = [2, 4, 5, 7];  //, 8, 9, 6, 10
+
+// for (var i = arr.length; i>=0; i--){
+//     console.log(arr[i])
+// }
+
+function arrayReverse(array){
+    var newarr = [];
+    for (var i = array.length; i >= 0; i--){
+        newarr.push(array[i])
+    }
+    return newarr
+}
+
+console.log(arrayReverse(arr))
