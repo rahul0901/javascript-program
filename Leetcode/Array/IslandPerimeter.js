97654d93f8887b0f9144186b4eaec82d82b1cab8
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-03 00:11:19 +0530

    adddddddddd

---
+++ Leetcode/Array/IslandPerimeter.js
@@ -0,0 +1,15 @@
+var islandPerimeter = function(grid) {
+    var c = 0;
+    for (let i = 0; i<grid.length; i++){
+        c = c + 1;
+        // var innerArray = [];
+        // innerArray = grid[i];
+        // for(let j = 0; j<innerArray.length; j++){
+
+        // }
+    }
+    c = c*4;
+    return c;
+};
+
+console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
