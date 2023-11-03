fb10d75da4ce29cc60e593770c232f892551d961
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-04 01:24:54 +0530

    add

--- Leetcode/Array/IslandPerimeter.js
+++ Leetcode/Array/IslandPerimeter.js
@@ -1,15 +1,35 @@
-var islandPerimeter = function(grid) {
-    var c = 0;
-    for (let i = 0; i<grid.length; i++){
-        c = c + 1;
-        // var innerArray = [];
-        // innerArray = grid[i];
-        // for(let j = 0; j<innerArray.length; j++){
+// var islandPerimeter = function(grid) {
+//     var c = 0;
+//     for (let i = 0; i<grid.length; i++){
+//         c = c + 1;
+//         // var innerArray = [];
+//         // innerArray = grid[i];
+//         // for(let j = 0; j<innerArray.length; j++){
 
-        // }
-    }
-    c = c*4;
-    return c;
-};
+//         // }
+//     }
+//     c = c*4;
+//     return c;
+// };
 
-console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
+// console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
+
+
+// var removeElement = function(nums, val) {
+//     var duplicates = [];
+//     var final = [];
+//     var c = 0;
+//     for(let i = 0; i<nums.length; i++){
+//         if(nums[i] === val){
+//             duplicates.push(nums[i])
+//         }
+//         else{
+//             final.push(nums[i])
+//             c = c + 1;
+//         }
+//     }
+//     return final;
+// };
+// console.log(removeElement([3,2,2,3], 3)) //[ 2, 2 ]
+// console.log(removeElement([0,1,2,2,3,0,4,2],2))  //[ 0, 1, 3, 0, 4 ]
+
